152ae90f78623cb95a0051c42c34a31e32bec972
Rick W <phial1020@gmail.com>
2012-02-08 06:13:13 -0700

    Implemented DecimalToBinary in Main
    Working conversion of a decimal to binary via the command line.
    Ready for release!

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -27,6 +27,21 @@ std::string DecimalToBinary(int number);
 int main(int argc, char *argv[]) {
     using namespace std;
 
+    // make sure the program is being used properly
+    if(argc != 2) {
+        cout << "Usage: dec2bin <number>\n";
+        return 1;
+    }
+
+    // convert the argument from a cstring to an int
+    int number = atoi(argv[1]);
+
+    // convert the number to binary
+    string binary = DecimalToBinary(number);
+
+    // print to screen
+    cout << binary << endl;
+
     return 0;
 }
 
@@ -35,6 +50,8 @@ int GetLargestMultipleOfTwo(int number) {
     int multiple = 1;
     bool found = false;
 
+    // keep looping as long as multiple * 2 is not greater than number
+    // if it is then stop and return multiple
     while(!found) {
         if(multiple * 2 <= number) 
             multiple *= 2;
@@ -48,10 +65,17 @@ int GetLargestMultipleOfTwo(int number) {
 // Convert decimal to binary
 std::string DecimalToBinary(int number) {
     using namespace std;
+    
+    // create a stream to treat as an output stream
     ostringstream oss;
     string str;
+    
+    // get multiple of two for the number
     int multiple = GetLargestMultipleOfTwo(number);
 
+    // start at the highest multiple and count down by half, each time checking
+    // that number - multiple isn't less than 0.  If it's greater than or equal
+    // to zero then reduce the number and output a '1', otherwise output '0'.
     for(; multiple >= 1; multiple /= 2) {
         if((number - multiple) >= 0) {
             number -= multiple;
@@ -60,6 +84,8 @@ std::string DecimalToBinary(int number) {
         else
             oss << "0";
     }
+
+    // get the string from the ostreamstring object
     str = oss.str();
     return str;
 }

9651eef6ca3c6fa271a7bf5e42689e91730a7002
Rick W <phial1020@gmail.com>
2012-02-07 21:53:10 -0700

    Updated gitignore and program skeleton.
    Added vim files to gitignore.
    Boiler plate includes and main function.

---
+++ dec2bin.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * dec2bin.cpp
+ *
+ * Rick W
+ * 02/07/2012
+ *
+ * Converts a number to binary from base 10.
+ *
+ *****************************************************************************/
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+// uses std namespace
+int main(int argc, char *argv[]) {
+    using namespace std;
+
+    return 0;
+}

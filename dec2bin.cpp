fb62f5ba995e81e1b9688f8abe49ddeb7800ab55
Rick W <phial1020@gmail.com>
2012-02-07 22:10:28 -0700

    GetLargestMultipleOfTwo function started.
    Function definiton created at top of file.  Implementation started at the
    bottom, does nothing.

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -12,9 +12,19 @@
 #include <sstream>
 #include <string>
 
+// Find the largest multiple of two that goes into a number
+// Takes a number to get multiple of.
+// Returns the largest multiple of two for a number.
+int GetLargestMultipleOfTwo(int number);
+
 // uses std namespace
 int main(int argc, char *argv[]) {
     using namespace std;
 
     return 0;
+}
+
+// Get largest multiple of two for a number
+int GetLargestMultipleOfTwo(int number) {
+
 }
